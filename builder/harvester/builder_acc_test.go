2f5ec5e02a294fe16c8dcec0cf55d31506a96af3
Ethan Nerone <enerone@riverpointtechnology.com>
2024-06-14 14:53:04 -0400

    wip

--- builder/harvester/builder_acc_test.go
+++ builder/harvester/builder_acc_test.go
@@ -1,19 +1,8 @@
 // Copyright (c) HashiCorp, Inc.
 // SPDX-License-Identifier: MPL-2.0
-// image download tests
-// 1. url is provided, checksum is provided
-// 2. url is provided, checksum is not provided
-// 3. url is not provided, checksum is not provided
-// 4. image with same name already exists in harvester, checksum is provided, url is provided but checksums differ, expect failure
-
-// TestAccBulder_imageDownloadWithChecksum
-// TestAccBulder_imageDownloadWithoutChecksum
-// TestAccBuild_imageExistsNoDownload
-// TestAccBuild_imageExistsWithDifferentChecksum
 package harvester
 
 import (
-	_ "embed"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -77,7 +66,7 @@ source "harvester" "foo" {
 
 // test3. url is not provided, checksum is not provided image exists. test not in use
 
-const testBuilderAccBasic_noChecksumNoUrlImage=`
+const testBuilderAccBasic_noChecksumNoUrlImage = `
 source "harvester" "foo" {
 
 	
@@ -102,7 +91,7 @@ source "harvester" "foo" {
 `
 
 // test4. image with same name already exists in harvester, checksum is provided, url is provided but checksums are not the same. test not in use
-const test4=`
+const test4 = `
 source "harvester" "foo" {
 
 	builder_source {
@@ -125,8 +114,9 @@ source "harvester" "foo" {
 	]
   }
 `
+
 // test5. url provided no checksum image exists. test not in use
-const test5=`
+const test5 = `
 source "harvester" "foo" {
 	builder_source {
 		url     = "http://cloud-images.ubuntu.com/releases/focal/release/ubuntu-20.04-server-cloudimg-amd64.img"
@@ -192,7 +182,6 @@ func TestAccBulder_imageDownloadWithChecksum(t *testing.T) {
 	acctest.TestPlugin(t, testCase)
 }
 
-
 // testBuilderAccBasic_imageNoChecksum. url is provided, checksum is not provided image does not exist attempt create. test currently in use
 func TestAccBulder_imageDownloadWithoutChecksum(t *testing.T) {
 	testCase := &acctest.PluginTestCase{
@@ -245,7 +234,7 @@ func TestAccBuild_imageExistsNoDownload(t *testing.T) {
 		Teardown: func() error {
 			return nil
 		},
-		Template:  testBuilderAccBasic_noChecksumNoUrlImage,
+		Template: testBuilderAccBasic_noChecksumNoUrlImage,
 		Type:     "harvester",
 		Check: func(buildCommand *exec.Cmd, logfile string) error {
 			if buildCommand.ProcessState != nil {
@@ -357,4 +346,3 @@ func TestAccBuild_imageExistsWithNoURLNoCheckSum(t *testing.T) {
 	}
 	acctest.TestPlugin(t, testCase)
 }
-
